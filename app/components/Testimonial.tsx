f8287d52495313f628c47ebff9ba8f65a06c367d
chimae <chimandukwe.r@gmail.com>
2025-01-09 19:50:28 +0000

    remove unused image

--- app/components/Testimonial.tsx
+++ app/components/Testimonial.tsx
@@ -9,18 +9,12 @@ interface TestimonialCardProps {
 }
 
 const Testimonial: React.FC<TestimonialCardProps> = ({
-  profileImg,
   testimonial,
   author,
   designation,
 }) => {
   return (
     <div className="border w-[100%] bg-[#f7f3ff] px-10 py-8 rounded-2xl relative">
-      {/* <Image
-        src={profileImg}
-        alt="profile-img"
-        className="w-10 h-10 rounded-full absolute top-[-15px]"
-      /> */}
       <p className="text-PrimaryBlue py-4 mt-8">{testimonial}</p>
       <div className="py-8">
         <p className="text-TextDarkBlue font-bold text-2xl">{`— ${author}`}</p>
